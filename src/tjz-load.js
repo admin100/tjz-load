6f306930f9d3b0142d198582e251d6583a817284
unknown <tjzaimee@163.com>
2014-03-05 09:26:22 +0800

    增加灰色半透明图层

--- src/tjz-load.js
+++ src/tjz-load.js
@@ -1,3 +1,7 @@
+/*************************
+*Apache License
+*Version 2.0, January 2004
+*************************/
 function TjzLoad(id){
 		this.id = id;
 		this.load = document.getElementById(this.id);
